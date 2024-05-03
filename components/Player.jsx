951075ec94cb6761b241296644cf30f18dadcdd3
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-05-03 23:16:32 +0530

    Adding Player Component

---
+++ components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const Player = ({ name, symbol }) => {
+  const [isEdit, SetEdit] = useState(false);
+  const [playerName, SetPlayerName] = useState(name);
+  const handleChange = (event) => {
+    SetPlayerName(event.target.value)
+  }
+  const handleClick = () => {
+    SetEdit(edit => !edit);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let button = 'edit'
+  if (isEdit)
+  {
+    editablePlayerName= <input type="text" required value={playerName} onChange={handleChange}></input>
+    button = 'save'
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick} >{button}</button>
+    </li>
+  );
+};
+
+export default Player;
